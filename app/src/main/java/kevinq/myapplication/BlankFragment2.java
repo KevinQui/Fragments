fb0155f5fe906e3f4eb42329e75be7336736c122
KevinQui <kqkevin16@gmail.com>
2017-04-02 22:59:33 -0300

    Fragments

--- app/src/main/java/kevinq/myapplication/BlankFragment2.java
+++ app/src/main/java/kevinq/myapplication/BlankFragment2.java
@@ -10,16 +10,10 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link BlankFragment2.OnFragmentInteractionListener} interface
- * to handle interaction events.
- */
 public class BlankFragment2 extends Fragment {
 
-    private OnFragmentInteractionListener mListener;
-String texto="";
+
+    String texto = "";
     TextView ver;
 
 
@@ -27,57 +21,10 @@ String texto="";
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-       View v =inflater.inflate(R.layout.fragment_blank2, container, false);
-        texto=getArguments().get("texto").toString();
-        ver=(TextView) v.findViewById(R.id.ver);
+        View v = inflater.inflate(R.layout.fragment_blank2, container, false);
+        texto = getArguments().get("texto").toString();
+        ver = (TextView) v.findViewById(R.id.ver);
         ver.setText(texto);
         return v;
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
-
-    public TextView getVer() {
-        return ver;
-    }
-
-    public void setVer(TextView ver) {
-        this.ver = ver;
     }
 }
--- app/src/main/java/kevinq/myapplication/MainActivity.java
+++ app/src/main/java/kevinq/myapplication/MainActivity.java
@@ -11,39 +11,24 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends Activity implements BlankFragment2.OnFragmentInteractionListener {
+public class MainActivity extends Activity {
     FragmentManager fm = getFragmentManager();
 
-    BlankFragment2 frag2;
-    Button bt;
-    EditText tx;
-    String texto = "";
-    TextView ver;
+    BlankFragment frag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bt = (Button) findViewById(R.id.enviar);
-        tx = (EditText) findViewById(R.id.tx);
-    }
-
-
-    @Override
-    public void onFragmentInteraction(Uri uri) {
-
-    }
-
-    public void Enviar(View v) {
-        ver = (TextView) findViewById(R.id.ver);
-        texto = tx.getText().toString();
-        frag2 = new BlankFragment2(texto);
-        FragmentTransaction t = fm.beginTransaction();
-        t.replace(R.id.main, frag2);
-        t.addToBackStack(null);
+        frag=new BlankFragment();
+        FragmentManager mf = getFragmentManager();
+        FragmentTransaction t = mf.beginTransaction();
+        t.add(R.id.main,frag);
         t.commit();
 
-
     }
+
+
+
 
 }
